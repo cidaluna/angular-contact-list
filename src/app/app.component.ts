f857d1e347e02e506e8238d3e764ded91ddc7e4b
Cida Luna <cidalunna@hotmail.com>
2024-10-27 19:44:39 -0300

    Add contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SpacerComponent } from './components/spacer/spacer.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent,
+    HeaderComponent, SpacerComponent, ContactComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-contact-list';
+  alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
 }
